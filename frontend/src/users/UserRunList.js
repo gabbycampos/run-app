44a113bcbfb68ff47b273874db19bd9f0b6f7535
Gabriela Campos <gabriela@Gabrielas-MacBook-Pro-2.local>
2021-08-07 19:19:37 -0700

    findAll

--- frontend/src/users/UserRunList.js
+++ frontend/src/users/UserRunList.js
@@ -2,39 +2,63 @@ import React, { useState, useEffect } from 'react';
 import RunAppApi from '../api/api';
 import UserRun from './UserRun';
 import LoadingSpinner from '../common/LoadingSpinner';
+import { useParams } from "react-router-dom";
 
 // routes: 
 // Shows list of runs from a user.
 
 function UserRunList() {
     const [runs, setRuns] = useState(null);
+    const { userId } = useParams();
+    // console.debug("UserRunList", "userId=", userId);
+    
+    // useEffect(function getRunListForUser() {
+    //     async function getRunList() {
+    //         // debugger;
+    //         setRuns(await RunAppApi.getRuns(userId));
+    //     }
+    //     getRunList();
+    // }, [userId]);
 
-    // useEffect to call api?
+    // useEffect(() => {
+    //     async function getRunList(userId) {
+    //         // debugger;
+    //         let runs = await RunAppApi.getRuns(userId);
+    //         setRuns(runs);
+    //     }
+    //     getRunList()
+    // }, [userId]);
+
     useEffect(function getRunsOnMount() {
-        getRunList();
-    }, []);
-
+        getRunList(userId);
+    }, [userId]);
     async function getRunList(userId) {
         // debugger;
+        // or let {runs} = 
         let runs = await RunAppApi.getRuns(userId);
         setRuns(runs);
     }
 
     if (!runs) return <LoadingSpinner />;
-
+    // debugger;
     return (
         <div>
             <h1>A users runs</h1>
             <div>
-                {runs.map(r => (
-                    <UserRun
-                        key={r.id}
-                        userId={r.userId}
-                        distance={r.distance}
-                        pace={r.pace}
-                        duration={r.duration}
-                    />
-                ))}
+                {runs.runs.length ? (
+                    <div>
+                        {runs.runs.map(r => (
+                            <UserRun 
+                                key={r.id}
+                                distance={r.distance}
+                                pace={r.pace}
+                                duration={r.duration} 
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <p>No runs available.</p>
+                )}
             </div>
         </div>
     )
